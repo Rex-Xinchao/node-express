b2e0ad75c578565e22d65dbcea50848790b9c28c
Rex-Xinchao <13601935905@163.com>
2019-10-21 15:20:29 +0800

    接口调整

--- model/person.js
+++ model/person.js
@@ -1,10 +1,10 @@
 const db = require('../datebase/db');
 module.exports = {
-  list: function (id, resolve, reject) {
+  list: function (params, resolve, reject) {
     let query = {
       sql: 'SELECT * FROM person WHERE id = ?',
       timeout: 4000,
-      values: [id]
+      values: [params.id]
     };
     db.row(query).then(function (dbRes) {
       resolve(dbRes);
